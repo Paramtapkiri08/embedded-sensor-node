0b5758cbee10ec0651c15e36e72651c82d4c49ed
Param <thisnutgotin@gmail.com>
2025-05-09 14:45:48 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,12 @@
+smart-temperature-logger/
+├── src/
+│   └── main.c or main.ino
+├── include/
+│   └── headers.h
+├── hardware/
+│   └── schematic.png
+├── docs/
+│   └── project-description.md
+├── .gitignore
+├── README.md
+└── LICENSE
